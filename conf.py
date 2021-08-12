97416b97ebb74324c74dc7c2bff159b10c723ff8
Jeremy Fischer <jlf599@users.noreply.github.com>
2021-08-12 15:38:49 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,1 +1,12 @@
 html_title = 'Jetstream2 Wiki'
+
+from sphinx.writers.html import HTMLTranslator
+class PatchedHTMLTranslator(HTMLTranslator):
+   def visit_reference(self, node):
+      if node.get('newtab') or not (node.get('target') or node.get('internal') 
+         or 'refuri' not in node):
+            node['target'] = '_blank'
+            super().visit_reference(node)
+
+def setup(app):
+    app.set_translator('html', PatchedHTMLTranslator)
