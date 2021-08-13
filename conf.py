515ca17aed28cf0ed94a1afd4d9de972890de0e2
Jeremy Fischer <jlf599@users.noreply.github.com>
2021-08-13 10:53:43 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,22 @@
 html_title = 'Jetstream2 Wiki'
 
+html_theme_options = {
+    'analytics_id': 'UA-204881722-1',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    #'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 5,
+    #'includehidden': True,
+    'titles_only': False
+}
+
 # Open external links in new tab/window
 from sphinx.writers.html import HTMLTranslator
 class PatchedHTMLTranslator(HTMLTranslator):
